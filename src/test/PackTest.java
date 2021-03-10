ee8ba8fc2404f4ce895240b3e91d1618335ba73e
LoganTann <logon313@hotmail.fr>
2021-03-10 15:32:45 +0100

    Ajout des tests unitaires pour la classe Pack

--- src/test/PackTest.java
+++ src/test/PackTest.java
@@ -5,44 +5,110 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import java.util.LinkedList;
+
 public class PackTest {
 
+	private Pack packDefault;
+	
     @Before
     public void setUp() throws Exception {
-
+        packDefault = new Pack();
     }
 
     @Test
     public void testConstants() {
-        assertEquals(Pack.DEFAULT_FIRST_CARD, 1);
-        assertEquals(Pack.DEFAULT_LAST_CARD, 60);
+        assertEquals(1, Pack.DEFAULT_FIRST_CARD);
+        assertEquals(60, Pack.DEFAULT_LAST_CARD);
     }
 
+    /**
+     * @see testConstants() pour justif valeurs 1 et 60
+     */
     @Test
-    public void getPack() {
-        Pack packDefault = new Pack();
+    public void constructors() {
         Pack packLitterals = new Pack(1, 60);
+        
+        // arrays defined by the constructor ?
         assertNotNull(packDefault.getPack());
         assertNotNull(packLitterals.getPack());
-    }
+        
+        // array sorted ?
+        LinkedList<Integer> packContent = packLitterals.getPack();
+        for (int i = 0; i < 60; i++) {
+        	assertTrue(packContent.get(i).equals(i + 1));
+        }
 
-    @Test
-    public void testDefaultConstructor() {
-        Pack packDefault = new Pack();
-        Pack packLitterals = new Pack(1, 60);
-
+        // default constructor should create a Pack(1, 60);
         assertArrayEquals(
-            packDefault.getPack().toArray(new Integer[0]),
-            packLitterals.getPack().toArray(new Integer[0])
+            packLitterals.getPack().toArray(new Integer[0]),
+            packDefault.getPack().toArray(new Integer[0])
         );
     }
-
+    
+    /**
+     * @see constructors() "array sorted ?"
+     */
     @Test
-    public void removeCard() {
-
+    public void pickCardMethods_and_getPackLenght() {
+    	// 60 cards by default
+    	assertEquals(60, packDefault.getPackLength());
+    	
+    	// when a card is picked, it returns its value
+    	assertEquals(3, packDefault.pickCard(2)); 
+    	assertEquals(4, packDefault.pickCard(2));
+    	// picked -> the value returned is also removed in the pack.
+    	assertEquals(60 - 2, packDefault.getPackLength());
+    	
+    	// pickFirstCard = pickCard(0)
+    	assertEquals(1, packDefault.pickFirstCard());
+    	
+    	// pickLastCard = pickCard( this.getPackLength() - 1)
+    	assertEquals(60, packDefault.pickLastCard());
+    	
+    	// current pack state : [2 5 6 7 ... 58 59]
     }
 
     @Test
-    public void exists() {
+    public void cloneConstructor() {
+        Pack packClone = new Pack(packDefault);
+        assertTrue(packDefault.getPack().equals(packClone.getPack()));
+        
+        packDefault.pickCard(0);
+        packDefault.pickCard(5);
+        packDefault.pickCard(0);
+        assertFalse(packDefault.getPack().equals(packClone.getPack()));
+        
+        // can be used for savestate system
+        packDefault = packClone;
+        assertTrue(packDefault.getPack().equals(packClone.getPack()));
+    }
+    
+    /**
+     * @see cloneConstructor() 
+     */
+    @Test
+    public void shuffle_and_exists() {
+        Pack sorted = new Pack();
+    	Pack toShuffle = new Pack();
+        assertTrue(sorted.getPack().equals(toShuffle.getPack()));
+        
+        toShuffle.shuffle();
+
+        assertFalse(sorted.getPack().equals(toShuffle.getPack()));
+        assertEquals(sorted.getPackLength(), toShuffle.getPackLength());
+        for (int card: sorted.getPack()) {
+        	assertTrue(toShuffle.exists(card));
+        }
+    }
+
+    @Test
+    public void isEmpty() {
+    	Pack verySmallPack = new Pack(1, 2); // content : [1 2]
+    	verySmallPack.pickFirstCard(); // clears it
+    	verySmallPack.pickLastCard();
+    	
+    	assertTrue(verySmallPack.isEmpty());
+    	assertEquals(-1, verySmallPack.pickLastCard());
     }
 }
